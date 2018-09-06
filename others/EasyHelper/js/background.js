71114020d949fd0d41e7a71db4a3a4fde990d254
dwf <cectdwf@126.com>
2018-09-06 10:48:30 +0800

    add flvcd

--- others/EasyHelper/js/background.js
+++ others/EasyHelper/js/background.js
@@ -1,4 +1,4 @@
-$PageReqs="";
+﻿$PageReqs="";
 chrome.webRequest.onBeforeRequest.addListener (
     function(dtl) {
         $PageReqs+="\n----------"+dtl.type+"----------\n"+dtl.url+"\n";
@@ -13,3 +13,18 @@ function clearPageReqs(){
 function getPageReqs(){
     return $PageReqs;
 }
+/*flvcd parse*/
+function onPageMenu(info, tab) {
+      var a = encodeURIComponent(info.pageUrl);
+      b='http://www.flvcd.com/parse.php?kw='+a;
+      chrome.tabs.create({ url: b });
+}
+
+function onLinkMenu(info, tab) {
+      var a = encodeURIComponent(info.linkUrl);
+      b='http://www.flvcd.com/parse.php?kw='+a;
+      chrome.tabs.create({ url: b });
+}
+var contexts = ["page","selection","link","editable","image","video","audio"];
+chrome.contextMenus.create({"title": "FLVCD解析本页", "contexts":[contexts[0]],"onclick": onPageMenu});
+chrome.contextMenus.create({"title": "FLVCD解析链接", "contexts":[contexts[2]],"onclick": onLinkMenu});
