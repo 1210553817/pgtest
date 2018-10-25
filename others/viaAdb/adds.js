256678e7ca88ac9dac14434a847f6c862284ae2c
dwfhub <cectdwf@126.com>
2018-10-26 03:13:48 +0800

    adds chg66

--- others/viaAdb/adds.js
+++ others/viaAdb/adds.js
@@ -1,20 +1,20 @@
 function $Q(e){return document.querySelectorAll(e);}
 var aps = $Q("div,a,img,iframe");
-alert("Length: "+aps.length);
+//alert("Length: "+aps.length);
 if(aps.length>0){
 	for(var i=0;i<aps.length;i++){
 		var itm = aps[i];
 		if(itm.href&&itm.href.indexOf("gif")>-1){
-			itm.style.display="none";
+			itm.parentNode.style.display="none";
 			continue;
 		}
 		if(itm.src&&itm.src.indexOf("gif")>-1){
-			itm.style.display="none";
+			itm.parentNode.style.display="none";
 			continue;
 		}
 		var atr=window.getComputedStyle(itm);
 		if(atr.backgroundImage&&atr.backgroundImage.indexOf("gif")>-1){
-			itm.style.display="none";
+			itm.parentNode.style.display="none";
 		}
 	}
 }
