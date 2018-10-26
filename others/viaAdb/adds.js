bd062d50daace6aec3b3430a24d7d5a97cdf3701
dwfhub <cectdwf@126.com>
2018-10-27 02:04:37 +0800

    adds chg77

--- others/viaAdb/adds.js
+++ others/viaAdb/adds.js
@@ -4,17 +4,30 @@ var aps = $Q("div,a,img,iframe");
 if(aps.length>0){
 	for(var i=0;i<aps.length;i++){
 		var itm = aps[i];
+		var ptm = itm.parentNode;
 		if(itm.href&&itm.href.indexOf("gif")>-1){
-			itm.parentNode.style.display="none";
+			itm.style.display="none";
+			if("span"==ptm.tagName)itm.parentNode.style.display="none";
 			continue;
 		}
 		if(itm.src&&itm.src.indexOf("gif")>-1){
-			itm.parentNode.style.display="none";
+			itm.style.display="none";
+			if("span"==ptm.tagName)itm.parentNode.style.display="none";
 			continue;
 		}
 		var atr=window.getComputedStyle(itm);
 		if(atr.backgroundImage&&atr.backgroundImage.indexOf("gif")>-1){
-			itm.parentNode.style.display="none";
+			itm.style.display="none";
+			if("span"==ptm.tagName){
+				itm.parentNode.style.display="none";
+				var chs=ptm.childNodes;
+				if(chs){
+					for(var i=0;i<chs.length;i++){
+						var chtm=chs[i];
+						chtm.style.display="none";
+					}
+				}
+			}
 		}
 	}
 }
